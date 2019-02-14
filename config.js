c309544dc704d1363e9bf01f2baea63378eea9bf
hafezbusra <hafezbusra@yahoo.com>
2019-02-15 06:44:41 +0800

    config

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  author: 'Hafez Busra', // Required
+  description: 'My Tech Journey',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: '#',
+    github: 'https://github.com/hafezbusra',
+    twitter: 'https://twitter.com/hafezbusra',
+    linkedin: 'https://www.linkedin.com/in/hafezbusra/',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://hafezbusra.netlify.com',
 };
